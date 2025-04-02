69605af588ea242e689dcfcedfc4d473956d96c8
shamjohari20 <shamjohari101@gmail.com>
2025-04-02 20:08:16 +0530

    adding loop Statement

--- Sample.java
+++ Sample.java
@@ -19,6 +19,13 @@ public class Sample {
             System.out.println("The sum is not greater than 10.");
         }
 
+	 // Example of a for loop.
+        System.out.println("Counting from 1 to 5:");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+
+
     }
 }
 
